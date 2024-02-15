2c7b95a1db96867e6d29b397e41e0acf3f0dc00b
Amr Noorwali <amr_a.h1424@hotmail.com>
2024-02-16 00:33:56 +0300

    initial commit

---
+++ js/myweb.js
@@ -0,0 +1,13 @@
+// const cardsContainer = document.querySelector(".box");
+
+// cardsContainer.addEventListener("click", (e) => {
+//   const target = e.target.closest(".card");
+
+//   if (!target) return;
+
+//   cardsContainer.querySelectorAll(".card").forEach((card) => {
+//     card.classList.remove("red");
+//   });
+
+//   target.classList.add("red");
+// });
